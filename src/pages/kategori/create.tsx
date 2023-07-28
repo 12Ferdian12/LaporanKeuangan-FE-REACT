b893963e64623a6d123897accb7f48437dda35f1
12Ferdian12 <Ferdianadinata07@gmail.com>
2023-07-28 20:12:51 +0700

    feat:Add reactHook Form

--- src/pages/kategori/create.tsx
+++ src/pages/kategori/create.tsx
@@ -1,5 +1,10 @@
 import { ToastContainer, toast } from "react-toastify";
+import { useForm, SubmitHandler } from "react-hook-form";
 import "react-toastify/dist/ReactToastify.css";
+
+type Inputs = {
+  CategoryName: string;
+};
 
 function CreateKategori() {
   const notify = () =>
@@ -14,38 +19,53 @@ function CreateKategori() {
       theme: "light",
     });
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmitCategory: SubmitHandler<Inputs> = (data) => {
+    notify;
+    console.log(data);
+  };
+
   return (
     <div className="container mx-auto px-4 py-10">
       <div className="border-b border-gray-900/10 pb-12">
         <div className="text-xl font-semibold  text-gray-900">
           Create Kategori
         </div>
-        <div className="mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-12">
-          <div className="sm:col-span-4">
-            <label className="block text-sm font-medium leading-6 text-gray-900">
-              Category Name Hallo
-            </label>
-            <div className="mt-2">
-              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                <input
-                  type="text"
-                  name="CategoryName"
-                  id="CategoryName"
-                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                  placeholder="janesmith"
-                  required
-                />
+        <form onSubmit={handleSubmit(onSubmitCategory)}>
+          <div className="mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-12">
+            <div className="sm:col-span-4">
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                Category Name
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input
+                    type="text"
+                    id="CategoryName"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="janesmith"
+                    {...register("CategoryName", {
+                      required: true,
+                      minLength: 7,
+                    })}
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <button
-          className="bg-amber-200 p-1 rounded mt-2"
-          onClick={notify}
-          type="submit"
-        >
-          Tambah
-        </button>
+          {errors.CategoryName && (
+            <div className={"text-red-700"}>This field is required</div>
+          )}
+
+          <button className="bg-amber-200 p-1 rounded mt-2" type="submit">
+            Tambah
+          </button>
+        </form>
         <ToastContainer />
       </div>
     </div>
